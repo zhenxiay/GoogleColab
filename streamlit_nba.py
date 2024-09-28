b7faf988bebb82ccbd6d668a6dfcc416318c47f2
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-09-28 17:47:02 +0200

    Update streamlit_nba.py
    Refactored code, added commentary in the code

--- streamlit_nba.py
+++ streamlit_nba.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#Definition of the function to get data
 def read_data(team,year):
     #year = dataset['Year-Wert']
     url = f'https://www.basketball-reference.com/teams/{team}/{year}.html'
@@ -14,11 +15,13 @@ cols = ['Player','Age','G','MP','PER','TS%','OBPM','DBPM','BPM','VORP']
 
 data = read_data('HOU',2024)[cols]
 
+#First part of the web app
 data
 
 st.header("Scatter plot | x- PER | y- TS%")
 st.divider()
 
+#Second part of the web app
 fig, ax = plt.subplots()
 ax.scatter(data['OBPM'], data['DBPM'])
 

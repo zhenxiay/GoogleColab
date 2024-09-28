01c9d1ed2b292f5f046c73da07edb908626eb4dd
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-09-28 18:08:14 +0200

    Changed annotation distance

--- streamlit_nba.py
+++ streamlit_nba.py
@@ -30,6 +30,6 @@ ax.scatter(x=data['OBPM'],
            alpha=0.5)
 
 for i, label in enumerate(data['Player']):
-    ax.annotate(label, (data['OBPM'][i] + 0.5, data['DBPM'][i] + 0.5))
+    ax.annotate(label, (data['OBPM'][i] + 0.15, data['DBPM'][i] + 0.15))
 
 st.pyplot(fig)

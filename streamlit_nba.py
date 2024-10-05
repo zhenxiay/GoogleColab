41520c297f32d765858be817ebc4455b8672223a
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-10-05 13:00:14 +0200

    Update streamlit_nba.py

--- streamlit_nba.py
+++ streamlit_nba.py
@@ -35,8 +35,8 @@ def scatter_plotly(df,team,year):
                  color="Player",
                  symbol="Tm")
 
-	for i in range(len(df)):
-    	fig.add_annotation(x=df['OBPM'].iloc[i]+0.1, y=df['DBPM'].iloc[i]+0.1, text=df['Player'].iloc[i])    
+	#for i in range(len(df)):
+    #	fig.add_annotation(x=df['OBPM'].iloc[i]+0.1, y=df['DBPM'].iloc[i]+0.1, text=df['Player'].iloc[i])    
 	return fig
 
 def scatter_matplotlib(df,team,year):        

0257a93fc3426c5f35455741c356161c0248e0f1
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-10-07 23:50:27 +0200

    Update streamlit_yfiance.py

--- streamlit_yfiance.py
+++ streamlit_yfiance.py
@@ -120,13 +120,13 @@ st.plotly_chart(fig)
 
 st.divider()
 #Second part of the web app
-fig_qf = stock_data.create_cf_fig()
+#fig_qf = stock_data.create_cf_fig()
 
 #fig_qf.iplot()
 
-st.plotly_chart(fig_qf)
+#st.plotly_chart(fig_qf)
 
-st.divider()
+#st.divider()
 
 #Third part of the web app
 

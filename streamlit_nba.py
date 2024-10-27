6cdf20d877a7776c38fa9bec5ee8e63e39d4f066
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2024-10-27 19:52:53 +0100

    Update streamlit_nba.py

--- streamlit_nba.py
+++ streamlit_nba.py
@@ -112,7 +112,7 @@ try:
    
 	data
     
-except KeyError:
+except IndexError:
     st.write('Adv data not availiable yet!')
 
 st.divider()
@@ -131,5 +131,5 @@ try:
 							team=team_selected,
 							year=year_selected)
 		st.plotly_chart(fig)
-except KeyError:
+except NameError:
     st.write('Adv data not availiable yet!')
